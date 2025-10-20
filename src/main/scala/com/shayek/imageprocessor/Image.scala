bb188bd9b72fabd47837a27c3c63a1ba2f733bfa
Shayek Ahmed <shayek.a@outlook.com>
2025-10-20 23:57:34 +0100

    add another crop example

--- src/main/scala/com/shayek/imageprocessor/Image.scala
+++ src/main/scala/com/shayek/imageprocessor/Image.scala
@@ -49,6 +49,6 @@ object Image {
 
   def main(args: Array[String]): Unit = {
 
-    loadAtResources("nature.jpg").crop(600, 100, 300, 300).saveAtResources("natureCroppedFix.jpg")
+    loadAtResources("city.jpg").crop(300, 200, 100, 100).saveAtResources("cityCropped.jpg")
   }
 }
